c1f5ae0b79b6a7a8a7b75ccc711118ff9d767ead
Mogamer4229 <orchardmyles12@gmail.com>
2026-09-15 21:38:40 -0600

    Implement login redirection for specific user
    Add user authentication check and redirect on login.
    
    Signed-off-by: Mogamer4229 <orchardmyles12@gmail.com>

--- login.js
+++ login.js
@@ -2,3 +2,8 @@
 let user = document.getElementById("Username");
 
 console.log(user);
+
+if(user="1A-30"){
+  location.replace("https://dada-dev-studios.github.io/Mogamers-CAD-MDT/home.html");
+  let signed_in = true;
+};
